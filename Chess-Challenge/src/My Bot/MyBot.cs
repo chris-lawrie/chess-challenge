41c6996dd3c7a3502475cc54843a04106d0739ce
Chris Lawrie <clawrie2020@gmail.com>
2023-09-04 19:49:45 -0400

    sketching out ideas

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -2,9 +2,22 @@
 
 public class MyBot : IChessBot
 {
+    /* Rough overview of what the Bot is doing..
+
+    - Starts with some uncommon 'book' opening
+    - Once out of the opening, determine the best move!
+        - Minmax with Alpha-Beta pruning? Monte Carlo Tree Search? Stochastic Gradient Descent?
+        - We will need to define some cost function to minimise?
+    - Once queens are off the board, enter 'end game' mode..?
+    */
+
+    // Piece values: null, pawn, knight, bishop, rook, queen, king
+    int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        System.Random rng = new ();
+        return moves[rng.Next(moves.Length)];
     }
 }
